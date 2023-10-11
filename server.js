d287953e27db14e7ac4cca87b77a30762e1ef8c2
0711509 <144315923+0711509@users.noreply.github.com>
2023-10-11 14:35:23 +0000

    pusher delete

--- server.js
+++ server.js
@@ -124,31 +124,3 @@ process.on("unhandledRejection", (error) => {
 // CLOUDINERY_SECRET = ERi4S49W2NilKcx9vAHOLQQ6-UI
 
 
-// =======================
-// Initialize Channels client
-let channels = new Pusher('app-key', {
-  cluster: 'cluster-region',
-});
-
-// Subscribe to the appropriate channel
-let channel = channels.subscribe('channel-name');
-
-// Bind a callback function to an event within the subscribed channel
-channel.bind('event-name', function (data) {
-  // Do what you wish with the data from the event
-});
-
-async function pushData(data) {
-  const res = await fetch('/api/channels-event', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(data),
-  });
-  if (!res.ok) {
-    console.error('failed to push data');
-  }
-}
-
-
